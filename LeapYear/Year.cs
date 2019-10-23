a4f9a37a2b7b473ee789e2643130bd2eecce82e3
slasmartres <SLasmartres@idt.pf>
2019-10-23 13:26:18 -1000

    leap year kata implementation

---
+++ LeapYear/Year.cs
@@ -0,0 +1,31 @@
+﻿namespace LeapYear
+{
+    public class Year
+    {
+        private readonly int _value;
+
+        public int Value {
+            get
+            {
+                return _value;
+            }
+        }
+
+        public Year(int value)
+        {
+            _value = value;
+        }
+
+        public bool IsLeap()
+        {
+            return IsDivisbleBy(100) 
+                ? IsDivisbleBy(400)
+                : IsDivisbleBy(4);
+        }
+
+        bool IsDivisbleBy(int value)
+        {
+            return _value % value == 0;
+        }
+    }
+}
---
+++ LeapYear/YearShould.cs
@@ -0,0 +1,59 @@
+using Xunit;
+
+namespace LeapYear
+{
+    public class YearShould
+    {
+        [Fact]
+        private void Be_A_Leap_Year_When_Year_Is_Divisible_By_400()
+        {
+            // Giwen
+            var sut = new Year(4000);
+
+            // When
+            var actual = sut.IsLeap();
+
+            // Then
+            Assert.True(actual);
+        }
+
+        [Fact]
+        private void Be_A_Leap_Year_When_Year_Is_Divisible_By_4_And_Is_Not_Divisible_By_100()
+        {
+            // Giwen
+            var sut = new Year(2016);
+
+            // When
+            var actual = sut.IsLeap();
+
+            // Then
+            Assert.True(actual);
+        }
+
+        [Fact]
+        private void Be_Not_A_Leap_Year_When_Year_Is_Divisible_By_100_And_Is_Not_Divisible_By_400()
+        {
+            // Giwen
+            var sut = new Year(4100);
+
+            // When
+            var actual = sut.IsLeap();
+
+            // Then
+            Assert.False(actual);
+        }
+
+        [Fact]
+        private void Be_Not_A_Leap_Year_When_Year_Is_Not_Divisible_By_4()
+        {
+            // Giwen
+            var sut = new Year(2015);
+
+            // When
+            var actual = sut.IsLeap();
+
+            // Then
+            Assert.False(actual);
+        }
+    }
+}
